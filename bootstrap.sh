c87464eb76215c736d89b8d67e4aa0243eb79180
HashedDan <georgedanielmangum@gmail.com>
2018-05-27 05:30:44 -0600

    Installation of golang on vm on boot

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
 
+sudo curl -O https://dl.google.com/go/go1.10.2.linux-amd64.tar.gz
+sudo tar -xvf go1.10.2.linux-amd64.tar.gz
+sudo mv go /usr/local
+echo 'export PATH=$PATH:/usr/local/go/bin' >>~/etc/profile
