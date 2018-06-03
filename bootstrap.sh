b40103af1b77109a38a8e10863ff0cc7d37871d6
HashedDan <georgedanielmangum@gmail.com>
2018-06-03 17:27:52 -0600

    bootstrap working minus package installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,19 @@
-GO_VERSION=1.3.3
+#!/bin/bash
+
+GO_VERSION=1.10.2
+
 echo 'Updating and installing Ubuntu packages...'
 sudo apt-get update
+
 echo 'Downloading go$GO_VERSION.linux-amd64.tar.gz'
-wget –quiet https://storage.googleapis.com/golang/go$GO_VERSION.linux-amd64.tar.gz
+wget –quiet https://dl.google.com/go/go$GO_VERSION.linux-amd64.tar.gz
+
 echo 'Unpacking go language'
 sudo tar -C /usr/local -xzf go$GO_VERSION.linux-amd64.tar.gz
+
 echo 'Setting up correct env. variables'
 echo "export GOPATH=/vagrant/" >> /home/vagrant/.bashrc
 echo "export PATH=$PATH:$GOPATH/bin:/usr/local/go/bin" >> /home/vagrant/.bashrc
+
+echo 'Downloading Iris Package'
+go get -u github.com/kataras/iris
