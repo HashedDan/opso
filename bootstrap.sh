aa0310fbdbb7d7aa9a2a3f7177e9404340c7bf52
HashedDan <georgedanielmangum@gmail.com>
2018-06-03 11:35:59 -0600

    Still working on correct golang setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,10 @@
-#!/usr/bin/env bash
-
-sudo curl -O https://dl.google.com/go/go1.10.2.linux-amd64.tar.gz
-sudo tar -xvf go1.10.2.linux-amd64.tar.gz
-sudo mv go /usr/local
-echo 'export PATH=$PATH:/usr/local/go/bin' >>~/etc/profile
+GO_VERSION=1.3.3
+echo 'Updating and installing Ubuntu packages...'
+sudo apt-get update
+echo 'Downloading go$GO_VERSION.linux-amd64.tar.gz'
+wget –quiet https://storage.googleapis.com/golang/go$GO_VERSION.linux-amd64.tar.gz
+echo 'Unpacking go language'
+sudo tar -C /usr/local -xzf go$GO_VERSION.linux-amd64.tar.gz
+echo 'Setting up correct env. variables'
+echo "export GOPATH=/vagrant/" >> /home/vagrant/.bashrc
+echo "export PATH=$PATH:$GOPATH/bin:/usr/local/go/bin" >> /home/vagrant/.bashrc
